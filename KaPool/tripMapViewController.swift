f1202a942d530934a31ee46a15f8bdc9e1ae78e0
Madel Asistio <madel.asistio@gmail.com>
2017-05-23 21:09:23 -0700

    updaated accepted rider

--- KaPool/tripMapViewController.swift
+++ KaPool/tripMapViewController.swift
@@ -13,7 +13,12 @@ import Alamofire
 
 class tripMapViewController: UIViewController, GMSMapViewDelegate {
     
+    @IBOutlet weak var requestorPic: GMSMapView!
     
+    @IBOutlet weak var statsView: UIView!
+    @IBOutlet weak var timeView: UIView!
+   
+    @IBOutlet weak var decisionView: UIView!
     var pickupLoc: String!
     var tripArr: [Trip] = []
     var otherRidersLoc: [GMSPlace] = []
@@ -92,6 +97,18 @@ class tripMapViewController: UIViewController, GMSMapViewDelegate {
                         print("other riders got")
                         self.fixMap(trips: self.otherRidersLoc, handleComplete: {
                             print ("map done")
+                            
+                            // brings views to front
+                            
+                            self.handleViews()
+                            
+                           
+                            
+                            
+                            
+                            
+                            
+                            
                         })
                     })
                 })
@@ -103,6 +120,23 @@ class tripMapViewController: UIViewController, GMSMapViewDelegate {
 
         // Do any additional setup after loading the view.
         }
+    }
+    
+    func handleViews() {
+
+        
+        self.decisionView.layer.shadowColor = UIColor.black.cgColor
+        self.decisionView.layer.shadowOpacity = 0.5
+        self.decisionView.layer.shadowOffset = CGSize(width: -1, height: 1)
+        self.mapView.bringSubview(toFront: self.decisionView)
+        
+        
+        self.statsView.layer.shadowColor = UIColor.black.cgColor
+        self.statsView.layer.shadowOpacity = 0.5
+        self.statsView.layer.shadowOffset = CGSize(width: -1, height: 1)
+        self.mapView.bringSubview(toFront: self.statsView)
+        
+        
     }
     
     func getOtherRiders(trips: [Trip], complete:@escaping () -> ()) {
@@ -163,7 +197,7 @@ class tripMapViewController: UIViewController, GMSMapViewDelegate {
         let ogMarker = GMSMarker()
         ogMarker.position = CLLocationCoordinate2D(latitude: origin!.coordinate.latitude, longitude: (origin?.coordinate.longitude)!)
         ogMarker.map = self.mapView
-        ogMarker.title = origin?.name
+        ogMarker.title = "Origin"
         ogMarker.icon = GMSMarker.markerImage(with: UIColor.green)
         
         
@@ -171,7 +205,7 @@ class tripMapViewController: UIViewController, GMSMapViewDelegate {
         destMarker.position = CLLocationCoordinate2D(latitude: destination!.coordinate.latitude, longitude: destination!.coordinate.longitude)
         destMarker.map = self.mapView
         
-        destMarker.title = destination?.name
+        destMarker.title = "Destination"
         
         path.add((origin?.coordinate)!)
         path.add((destination?.coordinate)!)
@@ -186,9 +220,49 @@ class tripMapViewController: UIViewController, GMSMapViewDelegate {
         
      //   getPolylineRoute(from: (origin?.coordinate)!, to: (destination?.coordinate)!)
        // drawPath(currentLocation: (origin?.coordinate)!, destinationLoc: (destination?.coordinate)!)
+        fetchMapData(from: (origin?.coordinate)!, to: (destination?.coordinate)!)
         
         handleComplete()
         
+        
+    }
+    
+    func fetchMapData(from: CLLocationCoordinate2D, to: CLLocationCoordinate2D) {
+        
+        let directionURL = "https://maps.googleapis.com/maps/api/directions/json?" +
+            "origin=\(from.latitude),\(from.longitude)&destination=\(to.latitude),\(to.longitude)&" +
+        "key=AIzaSyDpmL6EF08flh5k3sXyuCeVXm1MRX70MqE"
+        
+        
+        
+        Alamofire.request(directionURL).responseJSON
+            { response in
+                
+                if let JSON = response.result.value {
+                    
+                    let mapResponse: [String: AnyObject] = JSON as! [String : AnyObject]
+                    
+                    let routesArray = (mapResponse["routes"] as? Array) ?? []
+                    
+                    let routes = (routesArray.first as? Dictionary<String, AnyObject>) ?? [:]
+                    
+                    let overviewPolyline = (routes["overview_polyline"] as? Dictionary<String,AnyObject>) ?? [:]
+                    let polypoints = (overviewPolyline["points"] as? String) ?? ""
+                    let line  = polypoints
+                    
+                    self.addPolyLine(encodedString: line)
+                }
+        }
+        
+    }
+    
+    func addPolyLine(encodedString: String) {
+        
+        let path = GMSMutablePath(fromEncodedPath: encodedString)
+        let polyline = GMSPolyline(path: path)
+        polyline.strokeWidth = 5
+        polyline.strokeColor = .blue
+        polyline.map = mapView
         
     }
     /*
